ba1a98155f55f658262a40b95b5996b5b59c735e
Jonathan Rudd <jonathanrudd3@gmail.com>
2023-08-31 15:01:19 +0900

    rtk query api implemented

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -2,6 +2,8 @@ import { GoTrash } from "react-icons/go";
 import Button from "./Button";
 import { removeUser } from "../store";
 import { useThunk } from "../hooks/use-thunk";
+import ExpandablePanel from "./ExpandablePanel";
+import AlbumsList from "./AlbumsList";
 
 function UsersListItem({ user }) {
   const [doRemoveUser, isLoading, error] = useThunk(removeUser);
@@ -10,18 +12,18 @@ function UsersListItem({ user }) {
     doRemoveUser(user);
   }
 
-  return (
-    <div className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        <div className="flex flex-row items-center justify-between">
+  const header = <>
         <Button className="mr-3" loading={isLoading} onClick={handleClick}>
           <GoTrash />
         </Button>
         {error && <div>Error removing user</div>}
         {user.name}
-        </div>
-      </div>
-    </div>
+  </>
+
+  return (
+    <ExpandablePanel header={header}>
+      <AlbumsList user={user} />
+    </ExpandablePanel>
   );
 }
 
